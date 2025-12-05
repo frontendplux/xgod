1c87b0e71aff6119e97000aba87229348855ebba
frontendplux <frontendplux@gmail.com>
2025-12-04 20:48:57 -0800

    v1

--- function.php
+++ function.php
@@ -1,44 +1,27 @@
 <?php
-$path=parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
-$path=strtolower($path);
-$path=explode("/", $path);
-define('db_host','localhost');
-define('db_user','root');
-define('db_pass','');
-define('db_name','air9');
-$conn=new mysqli(db_host, db_user, db_pass) or die("unable to connect");
-$conn->query("create database if not exists ". db_name);
-$conn->select_db(db_name);
-include 'main.function.php';
-$data=json_decode(file_get_contents('php://input'), true);
-// print_r($path);
-switch ($path[1] ?? '') {
-    case '':
-    case 'home':
-        include 'main/index.php';
-        # code...
-        break;
-    
-    case 'create-password':
-        include 'main/create-password.php';
+include __dir__.'/main.php';
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        // handle GET request
+        echo "This is GET";
         break;
 
-    case 'api':
-        $auth=new authentication($conn);
-        switch ($path[2] ?? '') {
-            case 'login':
-                echo json_encode($auth->login($data));
-                break;
-            
-            default:
-                # code...
-                break;
+    case 'POST':
+        if($_POST['TYPE'] == 'login'){
+
         }
-        # code...
+
+       
         break;
-    
+
+    case 'PUT':
+        // handle PUT request
+        break;
+
+    case 'DELETE':
+        // handle DELETE request
+        break;
+
     default:
-        # code...
-        break;
+        echo "Unknown method";
 }
-?>
---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+  class auth{
+    public function __construct($conn){
+        $this->conn=$conn;
+    }
+
+    public function login($email){
+        
+    }
+  }
